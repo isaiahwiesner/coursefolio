b40172ee8ae337dff2c353bedbb462830e34987d
izzzzthewiz <isaiah.wiesner@yahoo.com>
2024-01-19 18:11:08 -0500

    404 page

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from datetime import datetime
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Request
 from fastapi.staticfiles import StaticFiles
-from models import db
 from routers.page_router import page_router
 from routers.api_router import api_router
+from models import db
+from extensions import templates
 
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(page_router, prefix="")
 app.include_router(api_router, prefix="/api")
+
+@app.exception_handler(404)
+async def not_found_exception_handler(request: Request, exc: HTTPException):
+    return templates.TemplateResponse(
+        "pages/not_found.html",
+        {"request": request},
+    )
 
 db.create_models()
